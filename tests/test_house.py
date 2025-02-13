fedcdf57ca45534776028295aec5f80d862137c8
nhathout <nhathout@bu.edu>
2025-02-13 13:29:44 -0500

    BUGFIX test_update_nonexistent_house

--- tests/test_house.py
+++ tests/test_house.py
@@ -55,9 +55,9 @@ def test_update_house(sample_owner, sample_house):
     assert retrieved.num_baths == 3
     assert retrieved.gps_location == (34.0522, -118.2437)
 
-def test_update_nonexistent_house(sample_house):
+def test_update_nonexistent_house(test_house):
     with pytest.raises(HouseNotFoundError):
-        update_house(sample_house)  # house was never created
+        update_house(test_house)  # house was never created
 
 def test_delete_house(sample_house):
     create_house(sample_house)
